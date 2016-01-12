8bb7ab745d2c646b57068eeba539b4588d353199
Sebastian Bensusan <sbensu@gmail.com>
2016-01-12 23:48:44 +0100

    Up to animals example

--- build.boot
+++ build.boot
@@ -1,11 +1,14 @@
 (set-env!
  :source-paths    #{"src/cljs"}
  :resource-paths  #{"resources"}
- :dependencies '[[adzerk/boot-cljs          "1.7.48-6"   :scope "test"]
-                 [adzerk/boot-cljs-repl     "0.2.0"      :scope "test"]
-                 [adzerk/boot-reload        "0.4.1"      :scope "test"]
+ :dependencies '[[adzerk/boot-cljs          "1.7.228-1"   :scope "test"]
+                 [adzerk/boot-cljs-repl     "0.3.0"      :scope "test"]
+                 [adzerk/boot-reload        "0.4.4"      :scope "test"]
                  [pandeiro/boot-http        "0.6.3"      :scope "test"]
-                 [org.clojure/clojurescript "1.7.122"]
+                 [com.cemerick/piggieback "0.2.1" :scope "test"]
+                 [weasel "0.7.0" :scope "test"]
+                 [org.clojure/tools.nrepl "0.2.12" :scope "test"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [org.omcljs/om "1.0.0-alpha22"]])
 
 (require
--- src/cljs/om_next/app.cljs
+++ src/cljs/om_next/app.cljs
@@ -5,4 +5,42 @@
 
 (enable-console-print!)
 
-(println "Hello world!")
+(def app-state
+  (atom {:app/title "Animals"
+         :animals/list [[1 "Ant"] [2 "Antelope"] [3 "Bird"] [4 "Cat"]]}))
+
+(defmulti read (fn [_ _ params] key))
+
+(defmethod read :default
+  [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      {:value value}
+      {:value :not-found})))
+
+(defmethod read :animals/list
+  [{:keys [state] :as env} key {:keys [start end]}]
+  {:value (subvec (:animals/list @state) start end)})
+
+(defui AnimalsList
+  static om/IQueryParams
+  (params [this] {:start 0 :end 10})
+  static om/IQuery
+  (query [this] '[:app/title (:animals/list {:start ?start :end ?end})])
+  Object
+  (render [this]
+          (let [{:keys [app/title animals/list]} (om/props this)]
+            (dom/div nil
+                     (dom/h2 nil title)
+                     (apply dom/ul nil
+                            (map (fn [[i name]]
+                                   (dom/li nil (str i ". " name))))
+                            list)))))
+
+(def reconciler
+  (om/reconciler {:state app-state
+                  :parser (om/parser {:read read})}))
+
+(defn init [] nil)
+
+(om/add-root! reconciler AnimalsList (gdom/getElement "container"))
