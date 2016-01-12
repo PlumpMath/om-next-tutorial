eab769dfbbbda9a6f8cee0594c8359d2686c42bb
Sebastian Bensusan <sbensu@gmail.com>
2016-01-12 21:54:35 +0100

    Start tutorial

---
+++ src/cljs/om_next/app.cljs
@@ -0,0 +1,8 @@
+(ns om-next.app
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]))
+
+(enable-console-print!)
+
+(println "Hello world!")
